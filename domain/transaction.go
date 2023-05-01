5163fe228b91fa26849ff4bd6bcb855fdad49d7d
adibaulia <adib.aulia@gli.id>
2023-05-01 11:23:12 +0700

    Add wallet contracts

--- domain/transaction.go
+++ domain/transaction.go
@@ -2,10 +2,10 @@ package domain
 
 type (
 	Transaction struct {
-		ID          string `json:"id"`
-		OwnedBy     string `json:"owned_by"`
-		Status      Status `json:"status"`
-		By          string `json:"by"`
-		DateCreated string `json:"date_created"`
+		ID          string       `json:"id"`
+		OwnedBy     string       `json:"owned_by"`
+		Status      WalletStatus `json:"status"`
+		By          string       `json:"by"`
+		DateCreated string       `json:"date_created"`
 	}
 )
--- domain/wallet.go
+++ domain/wallet.go
@@ -2,10 +2,22 @@ package domain
 
 type (
 	Wallet struct {
-		ID        string `json:"id"`
-		OwnedBy   string `json:"owned_by"`
-		Status    Status `json:"status"`
-		EnabledAt string `json:"enabled_at"`
-		Balance   int64  `json:"balance"`
+		ID        string       `json:"id"`
+		OwnedBy   string       `json:"owned_by"`
+		Status    WalletStatus `json:"status"`
+		EnabledAt string       `json:"enabled_at"`
+		Balance   int64        `json:"balance"`
+	}
+
+	WalletUseCase interface {
+		ChangeWalletStatus(string, WalletStatus) (*Wallet, error)
+		GetWalletBalance(string) (*Wallet, error)
+		UpdateMoneyWallet(string, TrxStatus, int64) (*Wallet, error)
+	}
+
+	WalletRepository interface {
+		CreateWallet(Wallet) error
+		GetWallet(string) (*Wallet, error)
+		UpdateWallet(Wallet) error
 	}
 )
